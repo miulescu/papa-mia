e47a7e52ee0870dacde89023f770f75ec14886c0
mihaimiulescu <mihai.miulescu@outlook.com>
2016-09-28 15:04:02 +0300

    restid

--- routes/things.js
+++ routes/things.js
@@ -48,7 +48,9 @@ router.get('/:id/categorii/', function(req, res){
            return;
          }   
  
-     connection.query("select cat_id from meniu_categorii_items where id_restaurant = " + req.params.id ,function(err,rows){
+     connection.query("select categorii.cat_nume from meniu_categorii_items 
+                inner join categorii on meniu_categorii_items.cat_id = categorii.cat_id 
+                where id_restaurant = " + req.params.id ,function(err,rows){
              connection.release();
              if(!err) {
                  res.json(rows);
