a4c825f573e8786dae5e5903585eb37495880cac
mihaimiulescu <mihai.miulescu@outlook.com>
2016-09-29 15:19:17 +0300

    adaugat si categoria

--- routes/things.js
+++ routes/things.js
@@ -60,7 +60,7 @@ router.get('/:id/categorii/', function(req, res){
 });
 
 router.get('/:id/categorii/:id/', function(req, res){
-    res.send('Ati ales categoria cu id ' + req.params.categorii.id );
+    res.send('Ati ales categoria cu id ' + req.params );
 
     // pool.getConnection(function(err,connection){
     //  if (err) {
