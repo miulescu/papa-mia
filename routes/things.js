264b6e75b652d6a2fece52b25da5994d2d118212
mihaimiulescu <mihai.miulescu@outlook.com>
2016-10-04 11:05:06 +0300

    item query

--- routes/things.js
+++ routes/things.js
@@ -79,7 +79,7 @@ router.get('/:id/categorii/:idcategorie/', function(req, res){
     });
 });
 
-router.get('/:id/categorii/:idcategorie/items/:iditem/', function(req, res){
+router.get("/:id/categorii/:idcategorie/items/:iditem/", function(req, res){
     // res.send('Ati ales itemul cu id ' + req.params.iditem );
 
     pool.getConnection(function(err,connection){
@@ -89,7 +89,7 @@ router.get('/:id/categorii/:idcategorie/items/:iditem/', function(req, res){
            return;
          }   
  
-     connection.query('select  * from meniu_categorii_items  m inner join items i on m.Item_id = i.Items_id  
+     connection.query('select  * from meniu_categorii_items  m inner join items i on m.Item_id = i.Items_id  \
                       where m.Item_id = ' + req.params.iditem , function(err,rows){
              connection.release();
              if(!err) {
