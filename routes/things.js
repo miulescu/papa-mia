017247f0c63dbe6cef35ab302b307b97da009afa
mihaimiulescu <mihai.miulescu@outlook.com>
2016-10-27 10:01:44 +0300

    in things cu left join pentru info ora

--- routes/things.js
+++ routes/things.js
@@ -24,7 +24,15 @@ router.get('/', function(req, res){
            return;
          }   
  
-	 connection.query("select * from restaurante",function(err,rows){
+	 // connection.query("select * from restaurante",function(err,rows){
+  //            connection.release();
+  //            if(!err) {
+  //                res.json(rows);
+  //            }           
+  //        });
+     connection.query("SELECT r.rest_nume, r.rest_logo_url, r.rest_descriere,\
+                      ri.info_ora_deschidere, ri.info_ora_inchidere FROM restaurante r \
+                      left join restaurante_info ri on r.rest_id = ri.idInfo",function(err,rows){
              connection.release();
              if(!err) {
                  res.json(rows);
